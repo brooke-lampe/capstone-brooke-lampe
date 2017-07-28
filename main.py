6306a59f41d6a66f08e096d9cb5c1931d20967ba
Brooke Lampe <belampe13@gmail.com>
2017-07-27 21:23:20 -0500

    Initial attempt to access patient information.

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from kivy.app import App
-from kivy.uix.label import Label
-
 from kivy.logger import Logger
-
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
 from openmrs import RESTConnection
 
-
-__app_package__ = 'edu.unl.cse.soft161.rest'
-__app__ = 'Rest'
-__version__ = '0.1'
-__flags__ = ['--bootstrap=sdl2', '--requirements=python2,kivy', '--permission=INTERNET', '--orientation=landscape']
-
-
 class RestApp(App):
+    recent_username = StringProperty("")
+
     def __init__(self, **kwargs):
         super(RestApp, self).__init__(**kwargs)
-        self.openmrs_connection = RESTConnection('localhost', 8080, 'admin', 'Admin123')
+
+    def connect(self):
+        self.openmrs_connection = RESTConnection('localhost', 8080, self.root.ids.username.text, self.root.ids.password.text)
+        self.root.ids.password.text = ""
 
     def load_locations(self):
         self.root.ids.results.clear_widgets()
@@ -26,9 +23,28 @@ class RestApp(App):
         results_layout = self.root.ids.results
         for result in response['results']:
             results_layout.add_widget(Label(text=result['display']))
+        recent_username = self.root.ids.username.text
+        #Do I need the above line?  Username should remain in the TextInput
+        #Later on, this will become a confirmation that the login was successful and will not be shown
 
     def on_locations_not_loaded(self, request, error):
         self.root.ids.results.add_widget(Label(text='[Failed to load locations]'))
+        Logger.error('RestApp: {error}'.format(error=error))
+        #Later on, this will become an error that the login was not successful and will not be shown
+
+    def load_patient(self):
+        self.root.ids.patient.clear_widgets()
+        print(self.root.ids.openmrs_id.text)
+        self.openmrs_connection.send_request('patient', None, self.on_patient_loaded,
+                                             self.on_patient_not_loaded, self.on_patient_not_loaded)
+
+    def on_patient_loaded(self, request, response):
+        patient_layout = self.root.ids.patient
+        for patient in response['patient']:
+            patient_layout.add_widget(Label(text=patient['display']))
+
+    def on_patient_not_loaded(self, request, error):
+        self.root.ids.patient.add_widget(Label(text='[Failed to load patient information.  Please try again.]'))
         Logger.error('RestApp: {error}'.format(error=error))
 
 
